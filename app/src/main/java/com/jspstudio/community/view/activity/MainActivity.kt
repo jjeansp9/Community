a4a5fe5fab3de2913a7483a17b6c1d626b1f62ed
jinsol <jjeansp9@gmail.com>
2024-02-01 00:16:11 +0900

    Initial commit

---
+++ app/src/main/java/com/jspstudio/community/view/activity/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.jspstudio.community.view.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jspstudio.community.R
+import com.jspstudio.community.databinding.ActivityMainBinding
+import com.jspstudio.community.util.KeepStateFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setNavigation()
+    }
+    private fun setNavigation() {
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navigator = KeepStateFragment(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+
+        navController.navigatorProvider.addNavigator(navigator)
+
+        navController.setGraph(R.navigation.nav_graph)
+
+        // MainActivity의 main_navi와 navController 연결
+        binding.mainNavi.setupWithNavController(navController)
+
+    }
+}
